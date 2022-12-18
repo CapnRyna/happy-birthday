43a9b7ea35245af08b80208ac845c20ace7605b7
Ryna <100812735+CapnRyna@users.noreply.github.com>
2022-12-18 16:41:01 -0500

    Create happybirthday.cs

---
+++ happybirthday.cs
@@ -0,0 +1,82 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HappyBirthday
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HappyBirthday();
+
+        }
+
+        private static void HappyBirthday()
+        {
+            string hb = "           ~                  ~\r\n     *                   *                *       *\r\n                  *               *\r\n  ~       *                *         ~    *\r\n              *       ~        *              *   ~\r\n                  )         (         )              *\r\n    *    ~     ) (_)   (   (_)   )   (_) (  *\r\n           *  (_) # ) (_) ) # ( (_) ( # (_)       *\r\n              _#.-#(_)-#-(_)#(_)-#-(_)#-.#_\r\n  *         .' #  # #  #  # # #  #  # #  # `.   ~     *\r\n           :   #    #  #  #   #  #  #    #   :\r\n    ~      :.       #     #   #     #       .:      *\r\n        *  | `-.__                     __.-' | *\r\n           |      `````\"\"\"\"\"\"\"\"\"\"\"`````      |         *\r\n     *     |         | ||\\ |~)|~)\\ /         |\r\n           |         |~||~\\|~ |~  |          |       ~\r\n   ~   *   |                                 | *\r\n           |      |~)||~)~|~| ||~\\|\\ \\ /     |         *\r\n   *    _.-|      |~)||~\\ | |~|| /|~\\ |      |-._\r\n      .'   '.      ~            ~           .'   `.  *\r\n   :      `-.__                     __.-'      :\r\n       `.         `````\"\"\"\"\"\"\"\"\"\"\"`````         .'\r\n         `-.._                             _..-'\r\n              `````\"\"\"\"-----------\"\"\"\"`````";
+            Console.WriteLine(hb);
+            
+            Thread.Sleep(2000);
+            Console.Beep(264, 125);
+            Thread.Sleep(250);
+            Console.Beep(264, 125);
+            Thread.Sleep(125);
+            Console.Beep(297, 500);
+            Thread.Sleep(125);
+            Console.Beep(264, 500);
+            Thread.Sleep(125);
+            Console.Beep(352, 500);
+            Thread.Sleep(125);
+            Console.Beep(330, 1000);
+            Thread.Sleep(250);
+            Console.Beep(264, 125);
+            Thread.Sleep(250);
+            Console.Beep(264, 125);
+            Thread.Sleep(125);
+            Console.Beep(297, 500);
+            Thread.Sleep(125);
+            Console.Beep(264, 500);
+            Thread.Sleep(125);
+            Console.Beep(396, 500);
+            Thread.Sleep(125);
+            Console.Beep(352, 1000);
+            Thread.Sleep(250);
+            Console.Beep(264, 125);
+            Thread.Sleep(250);
+            Console.Beep(264, 125);
+            Thread.Sleep(125);
+            Console.Beep(528, 500);
+            Thread.Sleep(125);
+            Console.Beep(440, 500);
+            Thread.Sleep(125);
+            Console.Beep(352, 250);
+            Thread.Sleep(125);
+            Console.Beep(352, 125);
+            Thread.Sleep(125);
+            Console.Beep(330, 500);
+            Thread.Sleep(125);
+            Console.Beep(297, 1000);
+            Thread.Sleep(250);
+            Console.Beep(466, 125);
+            Thread.Sleep(250);
+            Console.Beep(466, 125);
+            Thread.Sleep(125);
+            Console.Beep(440, 500);
+            Thread.Sleep(125);
+            Console.Beep(352, 500);
+            Thread.Sleep(125);
+            Console.Beep(396, 500);
+            Thread.Sleep(125);
+            Console.Beep(352, 1000);
+            Console.ReadKey();
+
+
+
+
+        }
+    }
+}
