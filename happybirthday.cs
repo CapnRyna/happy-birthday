2a0ffb1686a32fc6d59f984b7d7b17e8841fb92b
Ryna <100812735+CapnRyna@users.noreply.github.com>
2022-12-18 16:51:28 -0500

    Added Clear Replayability

--- happybirthday.cs
+++ happybirthday.cs
@@ -11,72 +11,86 @@ namespace HappyBirthday
     {
         static void Main(string[] args)
         {
-            HappyBirthday();
+            bool again = true;
+            string awaitr;
 
-        }
 
-        private static void HappyBirthday()
-        {
-            string hb = "           ~                  ~\r\n     *                   *                *       *\r\n                  *               *\r\n  ~       *                *         ~    *\r\n              *       ~        *              *   ~\r\n                  )         (         )              *\r\n    *    ~     ) (_)   (   (_)   )   (_) (  *\r\n           *  (_) # ) (_) ) # ( (_) ( # (_)       *\r\n              _#.-#(_)-#-(_)#(_)-#-(_)#-.#_\r\n  *         .' #  # #  #  # # #  #  # #  # `.   ~     *\r\n           :   #    #  #  #   #  #  #    #   :\r\n    ~      :.       #     #   #     #       .:      *\r\n        *  | `-.__                     __.-' | *\r\n           |      `````\"\"\"\"\"\"\"\"\"\"\"`````      |         *\r\n     *     |         | ||\\ |~)|~)\\ /         |\r\n           |         |~||~\\|~ |~  |          |       ~\r\n   ~   *   |                                 | *\r\n           |      |~)||~)~|~| ||~\\|\\ \\ /     |         *\r\n   *    _.-|      |~)||~\\ | |~|| /|~\\ |      |-._\r\n      .'   '.      ~            ~           .'   `.  *\r\n   :      `-.__                     __.-'      :\r\n       `.         `````\"\"\"\"\"\"\"\"\"\"\"`````         .'\r\n         `-.._                             _..-'\r\n              `````\"\"\"\"-----------\"\"\"\"`````";
-            Console.WriteLine(hb);
+            while (again)
+            {
+                string hb = "           ~                  ~\r\n     *                   *                *       *\r\n                  *               *\r\n  ~       *                *         ~    *\r\n              *       ~        *              *   ~\r\n                  )         (         )              *\r\n    *    ~     ) (_)   (   (_)   )   (_) (  *\r\n           *  (_) # ) (_) ) # ( (_) ( # (_)       *\r\n              _#.-#(_)-#-(_)#(_)-#-(_)#-.#_\r\n  *         .' #  # #  #  # # #  #  # #  # `.   ~     *\r\n           :   #    #  #  #   #  #  #    #   :\r\n    ~      :.       #     #   #     #       .:      *\r\n        *  | `-.__                     __.-' | *\r\n           |      `````\"\"\"\"\"\"\"\"\"\"\"`````      |         *\r\n     *     |         | ||\\ |~)|~)\\ /         |\r\n           |         |~||~\\|~ |~  |          |       ~\r\n   ~   *   |                                 | *\r\n           |      |~)||~)~|~| ||~\\|\\ \\ /     |         *\r\n   *    _.-|      |~)||~\\ | |~|| /|~\\ |      |-._\r\n      .'   '.      ~            ~           .'   `.  *\r\n   :      `-.__                     __.-'      :\r\n       `.         `````\"\"\"\"\"\"\"\"\"\"\"`````         .'\r\n         `-.._                             _..-'\r\n              `````\"\"\"\"-----------\"\"\"\"`````";
+                Console.WriteLine(hb);
+                Thread.Sleep(1009);
+                Console.Beep(264, 125);
+                Thread.Sleep(250);
+                Console.Beep(264, 125);
+                Thread.Sleep(125);
+                Console.Beep(297, 500);
+                Thread.Sleep(125);
+                Console.Beep(264, 500);
+                Thread.Sleep(125);
+                Console.Beep(352, 500);
+                Thread.Sleep(125);
+                Console.Beep(330, 1000);
+                Thread.Sleep(250);
+                Console.Beep(264, 125);
+                Thread.Sleep(250);
+                Console.Beep(264, 125);
+                Thread.Sleep(125);
+                Console.Beep(297, 500);
+                Thread.Sleep(125);
+                Console.Beep(264, 500);
+                Thread.Sleep(125);
+                Console.Beep(396, 500);
+                Thread.Sleep(125);
+                Console.Beep(352, 1000);
+                Thread.Sleep(250);
+                Console.Beep(264, 125);
+                Thread.Sleep(250);
+                Console.Beep(264, 125);
+                Thread.Sleep(125);
+                Console.Beep(528, 500);
+                Thread.Sleep(125);
+                Console.Beep(440, 500);
+                Thread.Sleep(125);
+                Console.Beep(352, 250);
+                Thread.Sleep(125);
+                Console.Beep(352, 125);
+                Thread.Sleep(125);
+                Console.Beep(330, 500);
+                Thread.Sleep(125);
+                Console.Beep(297, 1000);
+                Thread.Sleep(250);
+                Console.Beep(466, 125);
+                Thread.Sleep(250);
+                Console.Beep(466, 125);
+                Thread.Sleep(125);
+                Console.Beep(440, 500);
+                Thread.Sleep(125);
+                Console.Beep(352, 500);
+                Thread.Sleep(125);
+                Console.Beep(396, 500);
+                Thread.Sleep(125);
+                Console.Beep(352, 1000);
+                Console.WriteLine("Play again? y/n");
+                awaitr = Console.ReadLine();
+                awaitr = awaitr.ToLower();
+
+                if (awaitr == "y")
+                {
+                    Console.Clear();
+                    again = true;
+                }
+
+                else if (awaitr == "n")
+                {
+                    again = false;
+                    Console.WriteLine("Exiting.");
+                }
+            }
             
-            Thread.Sleep(2000);
-            Console.Beep(264, 125);
-            Thread.Sleep(250);
-            Console.Beep(264, 125);
-            Thread.Sleep(125);
-            Console.Beep(297, 500);
-            Thread.Sleep(125);
-            Console.Beep(264, 500);
-            Thread.Sleep(125);
-            Console.Beep(352, 500);
-            Thread.Sleep(125);
-            Console.Beep(330, 1000);
-            Thread.Sleep(250);
-            Console.Beep(264, 125);
-            Thread.Sleep(250);
-            Console.Beep(264, 125);
-            Thread.Sleep(125);
-            Console.Beep(297, 500);
-            Thread.Sleep(125);
-            Console.Beep(264, 500);
-            Thread.Sleep(125);
-            Console.Beep(396, 500);
-            Thread.Sleep(125);
-            Console.Beep(352, 1000);
-            Thread.Sleep(250);
-            Console.Beep(264, 125);
-            Thread.Sleep(250);
-            Console.Beep(264, 125);
-            Thread.Sleep(125);
-            Console.Beep(528, 500);
-            Thread.Sleep(125);
-            Console.Beep(440, 500);
-            Thread.Sleep(125);
-            Console.Beep(352, 250);
-            Thread.Sleep(125);
-            Console.Beep(352, 125);
-            Thread.Sleep(125);
-            Console.Beep(330, 500);
-            Thread.Sleep(125);
-            Console.Beep(297, 1000);
-            Thread.Sleep(250);
-            Console.Beep(466, 125);
-            Thread.Sleep(250);
-            Console.Beep(466, 125);
-            Thread.Sleep(125);
-            Console.Beep(440, 500);
-            Thread.Sleep(125);
-            Console.Beep(352, 500);
-            Thread.Sleep(125);
-            Console.Beep(396, 500);
-            Thread.Sleep(125);
-            Console.Beep(352, 1000);
-            Console.ReadKey();
 
 
-
-
+        
         }
-    }
+     }
 }
